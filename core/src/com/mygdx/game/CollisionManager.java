567863381fc24186b67eaf57adf848224ef66f82
Valerie <aeginana@gmail.com>
2023-02-16 21:48:08 +0800

    Collision Detection

--- core/src/com/mygdx/game/CollisionManager.java
+++ core/src/com/mygdx/game/CollisionManager.java
@@ -1,10 +1,7 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class CollisionManager implements ContactListener {
 
@@ -49,24 +46,40 @@ public class CollisionManager implements ContactListener {
         // Check to see if one of the fixtures belongs to a specific body type (e.g. PlayerFix)
         // Check if one of the fixtures belongs to a specific body type (e.g. the player)
 
+            //behaviourManager.onPlayerCollision();
+        System.out.println(fa.getBody().getType()+" has hit "+ fb.getBody().getType());
+        if(fa.getBody().getType() == BodyDef.BodyType.StaticBody){
+            this.shootUpInAir(fa, fb);
+        } else if(fb.getBody().getType() == BodyDef.BodyType.StaticBody){
+            this.shootUpInAir(fb, fa);
+        }else{
+            // neither a nor b are static so do nothing
+        }
 
-//            System.out.println(fa.getBody().getType()+" has hit "+ fb.getBody().getType());
         if(fa.getBody().getUserData() != null && fb.getBody().getUserData() != null) {
             String userDataA = (String)fa.getBody().getUserData();
             String userDataB = (String)fb.getBody().getUserData();
 
             if (userDataA.equals("PlayerFix") && userDataB.equals("TargetFix")
                     || userDataA.equals("TargetFix") && userDataB.equals("PlayerFix")){
-                // Create an instance of BehaviourManager class
+                // Create an instance of BehaviourManager OnPlayerCollision Method
                 //behaviourManager.onPlayerCollision();
                 System.out.println("Player collided with target");
-
+                // Figure out what type of collision is happening:
+                System.out.println(fa.getBody().getType()+" has hit "+ fb.getBody().getType());
             }
+
 
 
         }
 
+
         // Update world.setContactListener(new B2dContactListener(this)); to logic's constructor (b2dModel)
+    }
+
+    private void shootUpInAir(Fixture staticFixture, Fixture otherFixture) {
+        System.out.println("Adding Force Shoot Up In Air");
+        otherFixture.getBody().applyForceToCenter(new Vector2(-0,-200), true);
     }
 
     @Override
